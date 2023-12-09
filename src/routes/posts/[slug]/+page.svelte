03c97e6c4553721b8b3e3e65cb604833414c4876
Nick Cardoza <48460641+nacardo@users.noreply.github.com>
2023-12-08 23:51:40 -0600

    Destructure data on slug routes

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
 	export let data: PageServerData;
+	$: ({ title, author, content } = data);
 </script>
 
 <div class="container h-full mt-20 mx-auto flex justify-center">
 	<div class="space-y-10 flex flex-col md:w-3/4 lg:w-1/2">
-		{#if !data}
+		{#if !title}
 			Post not found.
 		{:else}
-			<h2 class="h2">{data.title}</h2>
-			<h3 class="h3">Author: {data.author}</h3>
+			<h2 class="h2">{title}</h2>
+			<h3 class="h3">Author: {author}</h3>
 
-			<div>{@html data.content}</div>
+			<div>{@html content}</div>
 		{/if}
 	</div>
 </div>
